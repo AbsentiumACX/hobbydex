d7781839482f373f27d8a1465563a0f3f51a884f
AbsentiumACX <jeroen@acxdev.nl>
2020-11-12 14:33:41 +0100

    Added the route for the characters section

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/characters', [App\Http\Controllers\CharactersController::class, 'index'])->name('characters');
