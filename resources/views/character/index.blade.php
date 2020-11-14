23125c9dac60817556c59d4a8e3e5e00470edf77
AbsentiumACX <jeroen@acxdev.nl>
2020-11-14 22:56:36 +0100

    added some styling

--- resources/views/character/index.blade.php
+++ resources/views/character/index.blade.php
@@ -13,10 +13,7 @@
                             </div>
                             <div class="float-right">
                                 <a class="btn btn-success" href="{{ route('character.create') }}" title="Create a project">
-                                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-plus-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                                        <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-                                    </svg>
+                                    <i class="fas fa-plus-square"></i>
                                 </a>
                             </div>
                         </div>
@@ -24,6 +21,11 @@
                 </div>
 
                 <div class="card-body">
+                    @if ($message = Session::get('success'))
+                        <div class="alert alert-success">
+                            <p>{{ $message }}</p>
+                        </div>
+                    @endif
                     <table class="table table-striped" width="100%">
                         <tr>
                             <th scope="col">{{ __('Character name') }}</th>
@@ -36,14 +38,15 @@
                                 <td>{{ $character->generation }}</td>
                                 <td>
                                     <form action="{{ route('character.destroy', $character->id) }}" method="POST">
+                                        <a href="{{ route('character.show', $project->id) }}" title="show">
+                                            <i class="fas fa-eye text-success  fa-lg"></i>
+                                        </a>
+
                                         @csrf
                                         @method('DELETE')
 
                                         <button type="submit" title="delete" style="border: none; background-color:transparent;">
-                                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                                                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                                            </svg>
+                                            <i class="fas fa-trash fa-lg text-danger"></i>
 
                                         </button>
                                     </form>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://kit.fontawesome.com/29c8bd6eec.js" crossorigin="anonymous"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
